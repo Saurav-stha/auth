b5b1b6b478db42dd711b875fd573d2b9956c188e
saurav <shresthsaurav8@gmail.com>
2024-07-13 00:01:09 +0545

    feat: connect to postgres

--- auth/settings.py
+++ auth/settings.py
@@ -12,6 +12,13 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+POSTGRES_PW = os.getenv('POSTGRES_PW')
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -78,9 +85,18 @@ WSGI_APPLICATION = 'auth.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
+
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE' : 'django.db.backends.postgresql',
+        'NAME' : 'railway',
+        'USER' : 'postgres',
+        'PASSWORD' : POSTGRES_PW ,
+        'HOST' : 'monorail.proxy.rlwy.net',
+        'PORT' : '10436'
+
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+
     }
 }
 
